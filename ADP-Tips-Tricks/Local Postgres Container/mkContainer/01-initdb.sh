b134770ce895ef66513610014546f56296357b4a
Galen Preston <59487077+grpreston@users.noreply.github.com>
2024-10-12 15:45:11 -0500

    checkpoint commit

--- ADP-Tips-Tricks/Local Postgres Container/mkContainer/01-initdb.sh
+++ ADP-Tips-Tricks/Local Postgres Container/mkContainer/01-initdb.sh
@@ -1,5 +1,28 @@
-CREATE USER ${APP};
-CREATE DATABASE docker;
-GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+#!/bin/bash
+set -e
+# export PGPASSWORD=$POSTGRES_PASSWORD;
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+  CREATE DATABASE $APP_DB_NAME;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
+  ALTER DATABASE $APP_DB_NAME OWNER TO $APP_DB_USER;
+  
+  \connect $APP_DB_NAME $APP_DB_USER
+  BEGIN;
+    CREATE TABLE IF NOT EXISTS event (
+	  id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
+	  aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
+	  event_data JSON NOT NULL,
+	  version INT,
+	  UNIQUE(aggregate_id, version)
+	);
+	CREATE INDEX idx_event_aggregate_id ON event (aggregate_id);
+  COMMIT;
+EOSQL
+
+# CREATE USER ${APP};
+# CREATE DATABASE docker;
+# GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
 
  
